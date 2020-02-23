07bfef466a894915e0197589cea196f7cff7eb46
Robert W <noyoureanerd@gmail.com>
2020-02-24 00:36:06 +1100

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-n = 1000
-
-for a in range(1, int(n/3 + 1)):
-    for b in range(a, int(n/2 + 1)):
-        c = n - a - b #c is remainder so that a + b + c == 1000
-
-        if a * a + b * b == c * c:
-            print(a * b * c)
